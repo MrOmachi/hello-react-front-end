8873d6cf800112adf995fa3bb77b2c997f130033
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-10-24 17:27:30 +0100

    Complete challenge

---
+++ hackerrank-solutions-master/counting-valleys.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    let elevation = 0;
+    let valleys = 0;
+    for (let i = 0; i < n; i++) {
+        if (s.charAt(i) == 'D') {
+            elevation--;
+        }
+        else {
+            if (elevation == -1) {
+                valleys++;
+            }
+            elevation++;
+        }
+    }
+    return valleys;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
+
