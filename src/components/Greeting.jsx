4b5d0afdd9f97921413c14113defe18b8897c503
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-05 04:04:52 +0100

    Setup components and display random greetongs

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMessage } from "../redux/greetings/greetingsSlice";
+
+const Greeting = () => {
+  const greetingShow = useSelector((state) => state.greetings.message);
+  const status = useSelector((state) => state.greetings.status);
+  const dispatch = useDispatch();
+  let content;
+  if (status === "success") {
+    content = greetingShow[0].message;
+  }
+
+  return (
+    <>
+      <h3>This are Random Messages:</h3>
+      <p>{content}</p>
+      <button type="button" onClick={() => dispatch(getMessage())}>
+        Display random greeting
+      </button>
+    </>
+  );
+};
+
+export default Greeting;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux/configureStore";
+import { getMessage } from "./redux/greetings/greetingsSlice";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+store.dispatch(getMessage());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
