6160e254ada55ea6008a4b3b4e6cacc5fdcb0ccb
Omachi John <omachi.john@yahoo.com>
2023-01-06 14:19:27 -0800

    Update readme

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import greetingsReducer from "./greetings/greetingsSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import greetingsReducer from './greetings/greetingsSlice';
 
 const store = configureStore({
   reducer: {
